eea3ac567bfe7affd9ce49e815905042f942af75
31337 <131667947+31337list@users.noreply.github.com>
2023-04-28 09:55:59 +1000

    bing in cli

--- bingsearch.ps1
+++ bingsearch.ps1
@@ -5,7 +5,7 @@ $searchTerm = Read-Host "Enter a search term"
 $searchUrl = "https://www.bing.com/search?q=$searchTerm"
 
 # Invoke web request to search URL and get HTML content
-$searchHtml = Invoke-WebRequest -Uri -UseBasicParsing $searchUrl
+$searchHtml = Invoke-WebRequest -Uri $searchUrl -UseBasicParsing
 
 # Parse HTML content to extract search results
 $searchResults = $searchHtml.ParsedHtml.GetElementsByClassName("b_algo")
